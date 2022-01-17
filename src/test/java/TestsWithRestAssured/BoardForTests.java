cc67f57f25ae3389bbd507749120dc8bb373531b
mpieron <mpieron@griddynamics.com>
2022-01-17 15:25:49 +0100

    Added tests for GET, PUT request

--- src/test/java/TestsWithRestAssured/BoardForTests.java
+++ src/test/java/TestsWithRestAssured/BoardForTests.java
@@ -1,35 +1,15 @@
 package TestsWithRestAssured;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Getter @Setter
 public class BoardForTests {
 
     private String name;
     private String desc;
     private boolean closed;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public boolean isClosed() {
-        return closed;
-    }
-
-    public void setClosed(boolean closed) {
-        this.closed = closed;
-    }
+    private String id;
 }
